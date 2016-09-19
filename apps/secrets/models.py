cc6af6f64163e59604780f2f14f6348ff9a9a878
james-moreno <bonsaimo7@gmail.com>
2016-09-19 10:48:33 -0700

    added form to html

--- apps/secrets/models.py
+++ apps/secrets/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Secrets(models.Model):
+    secret = models.CharField(max_length=255)
+    likes = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
